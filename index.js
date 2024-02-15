8a3be7a281d9680b24fb11698cdc905d0b5238f7
Kunal Lingwal <kunallingwal2@gmail.com>
2024-02-15 12:53:21 +0530

    fix:fixed cors

--- index.js
+++ index.js
@@ -4,23 +4,22 @@ const mongodb=require('./db');
 var cors = require('cors')
 
 app.use(cors({
-    origin:'https://nomnom-frontend.vercel.app',
-    methods:'GET,POST,PUT,DELETE',
+    origin:["https://nomnom-frontend.vercel.app"],
+    methods:["POST","GET"],
     credentials:true
 }));
 
-// app.use((req,res,next)=>{
-//     res.setHeader('Access-Control-Allow-Origin','https://nomnom-frontend.vercel.app');
-//     res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept');
-//     next();
-// })
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','https://nomnom-frontend.vercel.app');
+    res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept');
+    next();
+})
 
 app.use(express.json());
 app.use('/api',require('./Routes/create-user'));
 app.use('/api',require('./Routes/display-data'));
 
 mongodb();
-
 
 app.get('/',(req,res)=>{
     res.send('hello devloper')
